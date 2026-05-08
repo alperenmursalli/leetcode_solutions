df80df1de97113659470331e7f2b25d8e53e0d65
Alperen Mursalli <alperenmursalli0@gmail.com>
2026-05-08 13:58:58 +0300

    ValidAnagram

---
+++ transformAndConquer/242-ValidAnagram.java
@@ -0,0 +1,21 @@
+package transformAndConquer;
+
+import java.util.*;
+
+class ValidAnagram {
+    public boolean isValidAnagram(String s, String t) {
+        char[] s1 = s.toCharArray();
+        char[] t1 = t.toCharArray();
+
+        Arrays.sort(s1);
+        Arrays.sort(t1);
+
+        if (Arrays.equals(s1,t1)){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+}
+}

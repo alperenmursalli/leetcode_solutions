2ecd31f23463e79e5075cd4fc3a36662d863696f
Alperen Mursalli <alperenmursalli0@gmail.com>
2026-05-11 20:47:51 +0300

    JumpGame

--- greedyAlgorithm/55-JumpGame.java
+++ greedyAlgorithm/55-JumpGame.java
@@ -24,10 +24,8 @@ class JumpGame {
             if (i > farthest) {
                 return false;
             }
-
          
             farthest = Math.max(farthest, i + nums[i]);
-
        
             if (farthest >= nums.length - 1) {
                 return true;

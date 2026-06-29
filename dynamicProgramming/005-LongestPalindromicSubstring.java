9efb705276c4542455b3c74152b2c0ba7b0adb7a
Alperen Mursalli <alperenmursalli0@gmail.com>
2026-06-29 20:23:29 +0300

    arch

--- dynamicProgramming/005-LongestPalindromicSubstring.java
+++ dynamicProgramming/005-LongestPalindromicSubstring.java
@@ -6,7 +6,7 @@
 // Space Complexity: O(n^2)
 //
 // Explanation:
-// Use a DP table where dp[i][j]  tells whether s.substring(i, j + 1) is a palindrome.
+// Use a DP table where dp[i][j]  tells whether s.substring(i,j + 1) is a palindrome.
 // A substring is a pali  ndrome if i ts first and last characters match and the inside substring is also a palindrome.
 // dp[i][j] = s.charAt(i) ==  s.charAt(j) && (j - i < 3 || dp[i + 1][j - 1])
 //
